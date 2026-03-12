7e2980907663ee37759b9d7c3d45b3090f978012
Samuel Sateles Oliveira <samuelsateles8@gmail.com>
2026-03-11 23:44:36 -0300

    Modificação  CSS e implementação de novas páginas

--- Site com Sistema de Acompanhamento de episodios/Home/script.js
+++ Site com Sistema de Acompanhamento de episodios/Home/script.js
@@ -1,2 +1,7 @@
-print ("hellow world")
+const botao = document.getElementById("entrar");
+
+botao.addEventListener('click', () => {
+    console.log("Redirecionamento o usuário...");
+    window.location.href = "Cadastro/tela_de_cadastro.html";
+});
 
