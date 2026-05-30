9feeba34d1da10779f63fc1a02031c038d6b7a9b
Samu0K <samuelsateles8@gmail.com>
2026-05-29 23:46:26 -0300

    Estrutura, lógica e indentação estão todos prontos.

--- Site/Home/Login/login.js
+++ Site/Home/Login/login.js
@@ -8,22 +8,22 @@ async function login(event) {
 
     const email = document.getElementById("login-email").value
     const senha = document.getElementById("login-senha").value
+
+
+    const resposta = await fetch ("http://localhost:5000/login", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({ email, senha })
+    })
+
+    const dados = await resposta.json()
+
+    if (resposta.ok) {
+        localStorage.setItem("usuario", JSON.stringify(dados))
+        window.location.href = "../Main/index.html"
+    } else {
+        const msg = document.getElementById("msg-login")
+        msg.textContent = dados.erro
+        msg.style.display = "block"
+    }
 }
-
-const resposta = await fetch ("http://localhost:5000/login", {
-    method: "POST",
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify({ email, senha })
-})
-
-const dados = await resposta.json()
-
-if (resposta.ok) {
-    localStorage.setItem("usuario", JSON.stringify(dados))
-    window.location.href = "../Main/index.html"
-} else {
-    const msg = document.getElementById("msg-login")
-    msg.textContent = dados.erro
-    msg.style.display = "block"
-}
-
