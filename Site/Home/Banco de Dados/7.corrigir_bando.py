518923d5c40c178f7a4fd4342b054c968abaf014
Samu0K <samuelsateles8@gmail.com>
2026-06-28 00:05:55 -0300

    atualizando banco de dados

--- Site/Home/Banco de Dados/7.corrigir_bando.py
+++ Site/Home/Banco de Dados/7.corrigir_bando.py
@@ -4,9 +4,10 @@ conn = sqlite3.connect("banco_de_dados_de_usuario.bd")
 conn.execute("""CREATE TABLE IF NOT EXISTS usuarios (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nome TEXT NOT NULL,
-    apelido TEXT NOT NULL UNIQUE,
     email TEXT NOT NULL UNIQUE,
-    data_nascimento TEXT NOT NULL)""")
+    data_nascimento DATE NULL,
+    senha TEXT NOT NULL
+)""")
 
 conn.commit()
 conn.close()
